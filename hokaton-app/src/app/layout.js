46421d273722ba7e906518c6bd27040944720fa3
Ali Gaziev <67166171+GazievAli@users.noreply.github.com>
2024-10-04 15:27:32 +0300

    add new main files

--- hokaton-app/src/app/layout.js
+++ hokaton-app/src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="#" />
+        <link rel="icon" href="./assets/icons/main_icon.ico" />
       </head>
       <body>
         {children}
